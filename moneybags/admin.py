c96684feb6e4d06e15ac0249f7bed2a88fcc988f
Brad Montgomery <brad@bradmontgomery.net>
2013-07-19 22:29:02 -0500

    renaming things; pep8-ing the admin

--- moneybags/admin.py
+++ moneybags/admin.py
@@ -1,22 +1,34 @@
 from django.contrib import admin
-from moneybags import models 
+from moneybags import models
 
-class CofferAdmin(admin.ModelAdmin):
+
+class AccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'owner', )
-    search_fields = ('name', 'owner__username', 'owner__first_name', 'owner__last_name',)
+    search_fields = (
+        'name', 'owner__username', 'owner__first_name', 'owner__last_name'
+    )
     prepopulated_fields = {'slug': ('name',)}
+
 
 class TransactionAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
-    list_display = ('description', 'amount', 'coffer', 'date', 'recurring', 'pending', 'transaction_type') 
+    list_display = (
+        'description', 'amount', 'account', 'date', 'recurring', 'pending',
+        'transaction_type'
+    )
     list_filter = ('recurring', 'pending')
-    search_fields = ('description', 'coffer__name')
+    search_fields = ('description', 'account__name')
+
 
 class RecurringTransactionAdmin(admin.ModelAdmin):
     date_hierarchy = 'last_transaction_date'
-    list_display = ('due_date', 'coffer', 'description', 'desc_slug', 'frequency', 'frequency_start_date', 'last_transaction_date', )
-    search_fields = ('description', 'coffer__name')
+    list_display = (
+        'due_date', 'account', 'description', 'desc_slug', 'frequency',
+        'frequency_start_date', 'last_transaction_date'
+    )
+    search_fields = ('description', 'account__name')
 
-admin.site.register(models.Coffer, CofferAdmin)
+
+admin.site.register(models.Account, AccountAdmin)
 admin.site.register(models.Transaction, TransactionAdmin)
 admin.site.register(models.RecurringTransaction, RecurringTransactionAdmin)
