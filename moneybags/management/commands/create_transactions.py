782bb9bf0f261590660bacbe6626a69f96913901
Brad Montgomery <brad@bradmontgomery.net>
2013-07-19 22:46:49 -0500

    pep8

--- moneybags/management/commands/create_transactions.py
+++ moneybags/management/commands/create_transactions.py
@@ -1,6 +1,7 @@
 from django.core.management.base import NoArgsCommand
 from moneybags.models import create_transactions_due_today
 
+
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         create_transactions_due_today()
