ce87689f54f1852c87a3c777e81067d1c71887f5
Brad Montgomery <brad@bradmontgomery.net>
2013-07-19 22:46:23 -0500

    renaming things for forms

--- moneybags/forms.py
+++ moneybags/forms.py
@@ -1,52 +1,57 @@
 from django import forms
-from models import Coffer, Transaction, RecurringTransaction
+from models import Account, Transaction, RecurringTransaction
 
-class CofferForm(forms.ModelForm):
+
+class AccountForm(forms.ModelForm):
     class Meta:
-        model = Coffer
+        model = Account
         exclude = ('owner', 'slug', )
+
 
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
-        exclude = ('coffer', 'updated_on')
+        exclude = ('account', 'updated_on')
+
 
 class RecurringTransactionForm(forms.ModelForm):
     class Meta:
         model = RecurringTransaction
         fields = ('frequency_start_date', 'frequency', )
 
+
 class TransactionCheckBoxForm(forms.Form):
-    """ 
-    This is a form used to provide a general purpose ``checkbox`` widget for a ``Transaction``.
+    """ This is a form used to provide a general purpose ``checkbox`` widget
+    for a ``Transaction``.
     """
     value = forms.BooleanField(required=False)
     object_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
+
 
 def modelform_handler(request, form_klass, instance=None, commit=True):
     """
     A convenience function to handle ModelForm creation and
     Validation in a View.
-   
+
     Parameters:
     * ``request`` - the HttpRequest object
     * ``form_klass`` - a subclass of ModelForm
     * ``instance`` - an object that gets passed into the ``form_klass``
-    * ``commit`` - True or False; this is passed into the 
+    * ``commit`` - True or False; this is passed into the
                    ``form_klass``'s ``save`` method.
 
-    Returns a tuple: (``form``, ``object``) where ``form`` is 
+    Returns a tuple: (``form``, ``object``) where ``form`` is
     and instance of ``form_klass`` and ``object`` is the result
     of calling ``form.save()`` or ``None``.
-    """ 
+    """
     object = None
-    
+
     if request.method == "POST":
         if instance:
             form = form_klass(request.POST, instance=instance)
         else:
             form = form_klass(request.POST)
-        
+
         if form.is_valid():
             object = form.save(commit=commit)
     else:
@@ -54,6 +59,5 @@ def modelform_handler(request, form_klass, instance=None, commit=True):
             form = form_klass(instance=instance)
         else:
             form = form_klass()
-    
-    return (form, object)
 
+    return (form, object)
