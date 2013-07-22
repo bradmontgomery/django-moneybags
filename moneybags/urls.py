12302e00970b8a062b6a6642c5cdd567f3a4b005
Brad Montgomery <brad@bradmontgomery.net>
2013-07-22 17:46:30 -0500

    tweaked urlconf; using new url template tag

--- moneybags/urls.py
+++ moneybags/urls.py
@@ -2,39 +2,58 @@ from django.conf.urls.defaults import patterns, url
 from django.views.generic import TemplateView
 
 
+# /new/
+# /<account_slug>/update/
+# /<account_slug>/new/
+# /<account_slug>/transaction/<transaction_id>/
+# /<account_slug>/recurring/<transaction_id>/
+# /<account_slug>/<transaction_id>/
+# /<account_slug>/
+# /accounts/
+# /saved/
+# /
+
 urlpatterns = patterns('moneybags.views',
-    url(r'^moneybags/new/$',
+    url(r'^new/$',
         'account_create',
         name='moneybags-account-create'),
-    url(r'^moneybags/(?P<account_slug>.*)/update/$',
-        'account_update',
-        name='moneybags-account-update'),
-    url(r'^moneybags/(?P<account_slug>.*)/new/$',
-        'new_transaction',
-        name='moneybags-new-transaction'),
-    url(r'^moneybags/(?P<account_slug>.*)/transaction/(?P<transaction_id>\d+)/$',
-        'transaction_detail',
-        name='moneybags-transaction-detail'),
-    url(r'^moneybags/(?P<account_slug>.*)/recurring/(?P<recurring_transaction_id>\d+)/$',
-        'edit_recurring_transaction',
-        name='moneybags-edit-recurring-transaction'),
-    url(r'^moneybags/(?P<account_slug>.*)/(?P<transaction_id>\d+)/$',
-        'recurring_transaction',
-        name='moneybags-recurring-transaction'),
-    url(r'^moneybags/(?P<account_slug>.*)/$',
-        'account_detail',
-        name='moneybags-account-detail'),
-    url(r'^moneybags/$',
+
+    url(r'^accounts/$',
         'account_list',
         name='moneybags-account-list'),
+
+    url(r'^(?P<account_slug>.*)/update/$',
+        'account_update',
+        name='moneybags-account-update'),
+
+    url(r'^(?P<account_slug>.*)/new/$',
+        'new_transaction',
+        name='moneybags-new-transaction'),
+
+    url(r'^(?P<account_slug>.*)/transaction/(?P<transaction_id>\d+)/$',
+        'transaction_detail',
+        name='moneybags-transaction-detail'),
+
+    url(r'^(?P<account_slug>.*)/recurring/(?P<recurring_transaction_id>\d+)/$',
+        'edit_recurring_transaction',
+        name='moneybags-edit-recurring-transaction'),
+
+    url(r'^(?P<account_slug>.*)/(?P<transaction_id>\d+)/$',
+        'recurring_transaction',
+        name='moneybags-recurring-transaction'),
+
+    url(r'^(?P<account_slug>.*)/$',
+        'account_detail',
+        name='moneybags-account-detail'),
 )
 
 urlpatterns += patterns('',
     url(r'^saved/$',
         TemplateView.as_view(template_name='moneybags/saved.html'),
-        name='moneybags-saved'),
-
+        name='moneybags-saved'
+    ),
     url(r'^$',
         TemplateView.as_view(template_name='moneybags/default.html'),
-        name='moneybags-default'),
+        name='moneybags-default'
+    ),
 )
